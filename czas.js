0cd838baf4e5b00ce6f449626a7d821d4b0ccf0b
Zyza123 <53822449+Zyza123@users.noreply.github.com>
2022-07-03 12:37:34 +0200

    Add files via upload

---
+++ czas.js
@@ -0,0 +1,23 @@
+function data()
+{
+	var dzisiaj = new Date();
+	
+	var godzina = dzisiaj.getHours();
+	if(godzina<10)
+	{
+		godzina= "0" + godzina;
+	}
+	var minuta = dzisiaj.getMinutes();
+	if(minuta<10)
+	{
+		minuta= "0" + minuta;
+	}
+	var sekunda = dzisiaj.getSeconds();
+	if(sekunda<10)
+	{
+		sekunda= "0" + sekunda;
+	}
+	
+	document.getElementById("czas").innerHTML = godzina + ":" + minuta + ":" + sekunda;
+	setTimeout("data()",1000);
+}
